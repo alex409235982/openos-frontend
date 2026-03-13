be4ee26b11b58441ffbc092955f075b34cf7bfb1
alex409235982 <andreskemp853963963@gmail.com>
2026-03-13 12:06:23 -0400

    Swapped places for Terms and Privacy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ export default function Footer() {
         <span className="badge">Try Before You Install</span>
         <div className="row" style={{ alignItems: "center" }}>
           <Link to="/about">About</Link>
-          <Link to="/privacy">Privacy</Link>
           <Link to="/terms">Terms</Link>
+          <Link to="/privacy">Privacy</Link>
           <a href="https://github.com/alex409235982?tab=repositories" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
       </div>
