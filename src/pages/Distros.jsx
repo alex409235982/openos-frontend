e03c052368599ccc398ae6a8fbddbd0350b380ce
alex409235982 <andreskemp853963963@gmail.com>
2026-03-13 12:16:12 -0400

    Temporary Distros page until I write code

---
+++ src/pages/Distros.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Distros() {
+  return (
+    <div className="container">
+      <div className="card" style={{ textAlign: "center", padding: "48px 24px" }}>
+        <h1 className="h1">WIP</h1>
+      </div>
+    </div>
+  );
+}
